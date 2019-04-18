a27d03b16dbd6cf9c2857c1b44fee2ccbc22f071
Gonfidel <nicgeorge6@gmail.com>
2019-04-18 17:01:31 -0600

    remove registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   
   root 'catagories#home'
   get 'contactus', to: 'catagories#contactus'
-  devise_for :admins#, skip: [:registrations]
-  # as :admin do
-  #   get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-  #   get "/sign_up" => "devise/registrations#new", as: "new_admin_registration" # custom path to sign_up/registration
-  #   get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
-  #   put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
-  # end
+  devise_for :admins, skip: [:registrations]
+  as :admin do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_admin_registration" # custom path to sign_up/registration
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+  end
 
   resources :posts
   resources :catagories
