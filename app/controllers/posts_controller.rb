f55a0777e772a0f039b60390803c6509f183e844
Gonfidel <nicgeorge6@gmail.com>
2019-04-18 20:32:10 -0600

    prepare for presentation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,7 +56,8 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to :controller => 'catagories', :action => 'home', notice: 'Post was successfully deleted.' }
+      # format.html { redirect_to :controller => 'catagories', :action => 'home', notice: 'Post was successfully deleted.' }
+      format.html {redirect_back(fallback_location: root_path)}
       format.json { head :no_content }
     end
   end
