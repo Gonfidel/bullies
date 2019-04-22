54d61c9f29613306fc2fe79bac364cd26606c662
Gonfidel <nicgeorge6@gmail.com>
2019-04-22 11:11:18 -0600

    return to catagory page after add/edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to :controller => 'catagories', :action => 'home', notice: 'Post was successfully created.' }
+        format.html { redirect_to catagory_path(@post.catagory_id), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to :controller => 'catagories', :action => 'home', notice: 'Post was successfully updated.' }
+        format.html { redirect_to catagory_path(@post.catagory_id), notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
